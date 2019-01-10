071fcaf807e2cf9ab6a4de6f30e1e84fe6d84b03
Allan Jone.A <joeallan123@gmail.com>
2019-01-10 22:59:43 +0530

    The Node Event Emitter

--- app4.js
+++ app4.js
@@ -1,5 +1,31 @@
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.adder(5,7));
-console.log(stuff.counter(['anime','manga','movie']));
-console.log(stuff.adder(stuff.pi,10));
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person,events.EventEmitter);
+
+var person1 = new Person('Naruto');
+var person2 = new Person('Ichigo');
+var person3 = new Person('Luffy');
+var people = [person1,person2,person3];
+
+people.forEach(function(person){
+    person.on('speak',function(msg){
+        console.log(person.name + ' said ' + msg);
+    });
+});
+
+people.forEach(function(people){
+    people.emit('speak','Hi I am ' + people.name);
+});
+
+// var myEmitter = new events.EventEmitter();
+
+// myEmitter.on('someEvent',function(msg){
+//     console.log(msg);
+// });
+
+// myEmitter.emit('someEvent','the event is emitted');
