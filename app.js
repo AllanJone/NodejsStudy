9e77b58bb62195c6dd7f0931b60756bd3fc4edc3
Allan Jone.A <joeallan123@gmail.com>
2019-01-11 00:22:58 +0530

    Readable Streams

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req,res){
-    console.log('The request was made using the url : ' + req.url);
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('Hey fellow ninjas');
+var myReadStream = fs.createReadStream('readMe.txt');
+
+myReadStream.on('data',function(chunk){
+    console.log(('new chunked received' + chunk));
+      
 });
 
-server.listen(3000,'127.0.0.1');
-console.log('Yo dogs, you are now listening to port 3000');
+
+// var server = http.createServer(function(req,res){
+//     console.log('The request was made using the url : ' + req.url);
+//     res.writeHead(200,{'Content-Type': 'text/plain'});
+//     res.end('Hey fellow ninjas');
+// });
+
+// server.listen(3000,'127.0.0.1');
+// console.log('Yo dogs, you are now listening to port 3000');
